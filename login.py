48e531e0bfb277303b599cc7eb05a27efae184a6
parham-bakht <parhambakhtiari1380@gmail.com>
2026-09-23 15:03:24 +0330

    test
    test

---
+++ login.py
@@ -0,0 +1,16 @@
+from tkinter import *
+login_page = Tk()
+login_page.geometry("400x400")
+
+name_lbl = Label(login_page,text="Username",font=("arial",15))
+name_lbl.pack(pady=10)
+name_ent = Entry(login_page,font=("arial",15))
+name_ent.pack(pady=10)
+password_lbl = Label(login_page,text="Password",font=("arial",15))
+password_lbl.pack(pady=10)
+password_ent = Entry(login_page,font=("arial",15))
+password_ent.pack(pady=10)
+
+submit_btn = Button(login_page,text="Submit",font=("arial",15),bg="green",fg="white")
+submit_btn.pack(pady=10)
+login_page.mainloop()
